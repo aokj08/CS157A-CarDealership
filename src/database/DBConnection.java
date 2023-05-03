bc4061c7310997fc2e35c06e98f0786e5c7d8545
aokj08 <104790843+aokj08@users.noreply.github.com>
2023-05-03 12:22:19 -0700

    Initial commit

---
+++ src/database/DBConnection.java
@@ -0,0 +1,44 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+	
+	private static DBConnection dbConnection;
+	
+	private Connection connection;
+	
+	/*
+	 * Singleton design, the connection and all tables will be established just once.
+	 */
+	private DBConnection() { 
+		String url = "jdbc:sqlite:application.db";
+		try {
+			connection = DriverManager.getConnection(url);
+		}
+		catch (SQLException e) {
+			System.out.println("Error connecting to database.");
+			System.exit(1);
+		}
+	}
+	
+	/*
+	 * Returns singleton instance of DBConnection class, creates one if not already present
+	 */
+	public static DBConnection getDBConnection() {
+		if (dbConnection == null) {
+			dbConnection = new DBConnection();
+		}
+		return dbConnection;
+	}
+	
+	/*
+	 * Returns the Connection
+	 */
+	public Connection getConnection() {
+		return connection;
+	}
+	
+}
---
+++ src/database/DBManager.java
@@ -0,0 +1,51 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+
+public class DBManager {
+	
+	private static DBManager dbManager;
+	
+	private DBConnection dbConnection;
+	private Connection connection;
+	
+	/*
+	 * Connects to singleton instance of DBConnection and gets its connection
+	 */
+	private DBManager() {
+		dbConnection = DBConnection.getDBConnection();
+		connection = dbConnection.getConnection();
+	}
+	
+	/*
+	 * Get singleton instance of DBManager, creates a new instance if one does not exist
+	 */
+	public static DBManager getDBManager() {
+		if (dbManager == null) {
+			dbManager = new DBManager();
+		}
+		return dbManager;
+	}
+	
+	/*
+	 * Runs select query on database with given string
+	 */
+	public ResultSet query(String sqlQuery) {
+		try {
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(sqlQuery);
+			return rs;
+		}
+		catch (SQLException e) {
+			throw new RuntimeException(e.getMessage());
+        }
+	}
+	
+}
+
+	
+
