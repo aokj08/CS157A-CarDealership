4455b163c752e843e686fd8684656d54f8f85d77
Jisoo Kim <jisoo.kim1710@gmail.com>
2023-05-07 21:02:42 -0700

    Created Employee UI for the dealership

---
+++ src/gui/Empdealershiplookup.java
@@ -0,0 +1,96 @@
+package gui;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Empdealershiplookup {
+
+    private JFrame frame;
+    private JTable table;
+    private JButton backButton;
+    private JTextField searchField;
+    private Object[] row1, row2, row3;
+
+    public Empdealershiplookup() {
+        // Create a JFrame
+        frame = new JFrame("Dealerships in Bay Area - Employee View");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 400);
+        frame.setLocationRelativeTo(null); // Center the window on the screen
+
+        // Create a JPanel for the header
+        JPanel headerPanel = new JPanel();
+        JLabel headerLabel = new JLabel("Dealerships in the Bay Area - Employee View");
+        headerLabel.setFont(new Font("Arial", Font.BOLD, 18));
+        headerPanel.add(headerLabel);
+
+        // Create a JPanel for the main content
+        JPanel mainPanel = new JPanel(new BorderLayout());
+
+        // Create a JTable to display the dealerships
+        String[] columns = { "Dealership ID", "Street Address", "City", "Zip Code", "State", "Country" };
+        DefaultTableModel model = new DefaultTableModel(columns, 0);
+        table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+        mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        // Add some sample data to the table
+        row1 = new Object[] { "1", "123 Main St", "San Francisco", "94102", "CA", "USA" };
+        row2 = new Object[] { "2", "456 Oak St", "San Jose", "95134", "CA", "USA" };
+        row3 = new Object[] { "3", "789 Elm St", "Oakland", "94601", "CA", "USA" };
+        model.addRow(row1);
+        model.addRow(row2);
+        model.addRow(row3);
+
+        // Create a JPanel for the search bar
+        JPanel searchPanel = new JPanel();
+        searchField = new JTextField(20);
+        JButton searchButton = new JButton("Search ZIP Code");
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String zipCode = searchField.getText();
+                // Update the table with the search results
+                model.setRowCount(0); // clear the table
+                Object[] row = { "4", "789 Maple St", "Sunnyvale", "94086", "CA", "USA" };
+                model.addRow(row);
+            }
+        });
+        searchPanel.add(searchField);
+        searchPanel.add(searchButton);
+        mainPanel.add(searchPanel, BorderLayout.NORTH);
+
+        // Create a JPanel for the back button
+        JPanel backPanel = new JPanel();
+        backButton = new JButton("Back");
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Return to the main dealership list
+                // Clear the search field
+                searchField.setText("");
+                // Update the table with all the dealerships
+                model.setRowCount(0); // clear the table
+                model.addRow(row1);
+                model.addRow(row2);
+                model.addRow(row3);
+                // Open the employee
+                new Employeepage();
+                // Close the current page
+                frame.dispose();
+            }
+        });
+        backPanel.add(backButton);
+        mainPanel.add(backPanel, BorderLayout.SOUTH);
+
+        // Add the header and main content to the frame
+        frame.add(headerPanel, BorderLayout.NORTH);
+        frame.add(mainPanel, BorderLayout.CENTER);
+
+        // Make the frame visible
+        frame.setVisible(true);
+    }
+}
--- src/gui/Homepage.java
+++ src/gui/Homepage.java
@@ -12,7 +12,7 @@ public class Homepage {
     private DBManager dbM;
 
     public Homepage() {
-        dbM = DBManager.getDBManager();  
+        dbM = DBManager.getDBManager();
         JFrame frame = new JFrame("Dealership Homepage");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(500, 300);
@@ -40,23 +40,24 @@ public class Homepage {
         JButton loginButton = new JButton("Log In");
         loginButton.addActionListener(e -> {
             String email = usernameField.getName(), password = passwordField.getName();
-            if(accountTypeDropdown.getSelectedItem() == "Employee") {
-                ResultSet rs = dbM.query(String.format("SELECT email, password FROM Employee WHERE email = '%s' AND password = '%s';", email, password));
-                if(rs != null) {
-                    //new EmployeePage();
+            if (accountTypeDropdown.getSelectedItem() == "Employee") {
+                ResultSet rs = dbM.query(
+                        String.format("SELECT email, password FROM Employee WHERE email = '%s' AND password = '%s';",
+                                email, password));
+                if (rs != null) {
+                    new Employeepage();
+                } else {
+                    // Display Error message
                 }
-                else {
-                    //Display Error message
-                }
-            }
-            else {
-                ResultSet rs = dbM.query(String.format("SELECT email, password FROM Customer WHERE email = '%s' AND password = '%s';", email, password));
-                if(rs != null) {
-                    //implement closing current window if have time
-                    new Carbrowsingpage(); //Customer page
-                }
-                else {
-                    //Display Error message
+            } else {
+                ResultSet rs = dbM.query(
+                        String.format("SELECT email, password FROM Customer WHERE email = '%s' AND password = '%s';",
+                                email, password));
+                if (rs != null) {
+                    // implement closing current window if have time
+                    new Carbrowsingpage(); // Customer page
+                } else {
+                    // Display Error message
                 }
             }
         });
@@ -87,69 +88,70 @@ public class Homepage {
         frame.add(mainPanel, BorderLayout.CENTER);
 
         // Make the frame visible
-        frame.setVisible(true);      
+        frame.setVisible(true);
     }
-//     public static void main(String[] args) {
-//         // Create a JFrame
-//         JFrame frame = new JFrame("Dealership Homepage");
-//         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//         frame.setSize(500, 300);
-//         frame.setLocationRelativeTo(null); // Center the window on the screen
+    // public static void main(String[] args) {
+    // // Create a JFrame
+    // JFrame frame = new JFrame("Dealership Homepage");
+    // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    // frame.setSize(500, 300);
+    // frame.setLocationRelativeTo(null); // Center the window on the screen
 
-//         // Create a JPanel for the header
-//         JPanel headerPanel = new JPanel();
-//         JLabel headerLabel = new JLabel("Welcome to the Dealership!");
-//         headerLabel.setFont(new Font("Arial", Font.BOLD, 24));
-//         headerPanel.add(headerLabel);
+    // // Create a JPanel for the header
+    // JPanel headerPanel = new JPanel();
+    // JLabel headerLabel = new JLabel("Welcome to the Dealership!");
+    // headerLabel.setFont(new Font("Arial", Font.BOLD, 24));
+    // headerPanel.add(headerLabel);
 
-//         // Create a JPanel for the main content
-//         JPanel mainPanel = new JPanel(new BorderLayout());
-//         JLabel mainLabel = new JLabel("Please log in or sign up below:");
-//         mainPanel.add(mainLabel, BorderLayout.NORTH);
+    // // Create a JPanel for the main content
+    // JPanel mainPanel = new JPanel(new BorderLayout());
+    // JLabel mainLabel = new JLabel("Please log in or sign up below:");
+    // mainPanel.add(mainLabel, BorderLayout.NORTH);
 
-//         // Create a JPanel for the login form
-//         JPanel loginPanel = new JPanel(new GridLayout(4, 2, 5, 5));
-//         JLabel accountTypeLabel = new JLabel("Account Type:");
-//         JComboBox<String> accountTypeDropdown = new JComboBox<>(new String[] { "Employee", "Customer" });
-//         JLabel usernameLabel = new JLabel("Email:");
-//         JLabel passwordLabel = new JLabel("Password:");
-//         JTextField usernameField = new JTextField();
-//         JPasswordField passwordField = new JPasswordField();
-//         JButton loginButton = new JButton("Log In");
-//         loginButton.addActionListener(e -> {
-//             String email = usernameField.getName(), password = passwordField.getName();
-//             if(accountTypeDropdown.getSelectedItem() == "Employee") {
-//                 // ResultSet rs = dbM
-//             }
-//             new Carbrowsingpage(); //Customer page
-//         });
-//         loginPanel.add(accountTypeLabel);
-//         loginPanel.add(accountTypeDropdown);
-//         loginPanel.add(usernameLabel);
-//         loginPanel.add(usernameField);
-//         loginPanel.add(passwordLabel);
-//         loginPanel.add(passwordField);
-//         loginPanel.add(new JLabel(""));
-//         loginPanel.add(loginButton);
-//         mainPanel.add(loginPanel, BorderLayout.CENTER);
+    // // Create a JPanel for the login form
+    // JPanel loginPanel = new JPanel(new GridLayout(4, 2, 5, 5));
+    // JLabel accountTypeLabel = new JLabel("Account Type:");
+    // JComboBox<String> accountTypeDropdown = new JComboBox<>(new String[] {
+    // "Employee", "Customer" });
+    // JLabel usernameLabel = new JLabel("Email:");
+    // JLabel passwordLabel = new JLabel("Password:");
+    // JTextField usernameField = new JTextField();
+    // JPasswordField passwordField = new JPasswordField();
+    // JButton loginButton = new JButton("Log In");
+    // loginButton.addActionListener(e -> {
+    // String email = usernameField.getName(), password = passwordField.getName();
+    // if(accountTypeDropdown.getSelectedItem() == "Employee") {
+    // // ResultSet rs = dbM
+    // }
+    // new Carbrowsingpage(); //Customer page
+    // });
+    // loginPanel.add(accountTypeLabel);
+    // loginPanel.add(accountTypeDropdown);
+    // loginPanel.add(usernameLabel);
+    // loginPanel.add(usernameField);
+    // loginPanel.add(passwordLabel);
+    // loginPanel.add(passwordField);
+    // loginPanel.add(new JLabel(""));
+    // loginPanel.add(loginButton);
+    // mainPanel.add(loginPanel, BorderLayout.CENTER);
 
-//         // Create a JPanel for the sign up link
-//         JPanel signupPanel = new JPanel();
-//         JLabel signupLabel = new JLabel("Don't have an account? ");
-//         JButton signupButton = new JButton("Sign Up");
-//         signupButton.addActionListener(e -> {
-//             // Open Signup JFrame
-//             new Signup();
-//         });
-//         signupPanel.add(signupLabel);
-//         signupPanel.add(signupButton);
-//         mainPanel.add(signupPanel, BorderLayout.SOUTH);
+    // // Create a JPanel for the sign up link
+    // JPanel signupPanel = new JPanel();
+    // JLabel signupLabel = new JLabel("Don't have an account? ");
+    // JButton signupButton = new JButton("Sign Up");
+    // signupButton.addActionListener(e -> {
+    // // Open Signup JFrame
+    // new Signup();
+    // });
+    // signupPanel.add(signupLabel);
+    // signupPanel.add(signupButton);
+    // mainPanel.add(signupPanel, BorderLayout.SOUTH);
 
-//         // Add the header and main content to the frame
-//         frame.add(headerPanel, BorderLayout.NORTH);
-//         frame.add(mainPanel, BorderLayout.CENTER);
+    // // Add the header and main content to the frame
+    // frame.add(headerPanel, BorderLayout.NORTH);
+    // frame.add(mainPanel, BorderLayout.CENTER);
 
-//         // Make the frame visible
-//         frame.setVisible(true);
-//     }
+    // // Make the frame visible
+    // frame.setVisible(true);
+    // }
 }
