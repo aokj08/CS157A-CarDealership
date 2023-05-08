ddc8647a01f63354f5d565fcc13ae0e23c74ca5d
Jisoo Kim <jisoo.kim1710@gmail.com>
2023-05-07 17:09:46 -0700

    made GUI for the dealership

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,15 +7,14 @@ import database.DBManager;
 
 public class Main {
 
-	
-	private DBManager dbM; //We use dbM as a handle to the database.
+	private DBManager dbM; // We use dbM as a handle to the database.
 
 	/**
 	 * Public constructor, instantiating mainObj and dbM.
 	 */
 	public Main() {
 		dbM = DBManager.getDBManager();
-		
+
 	}
 
 	/**
@@ -23,35 +22,31 @@ public class Main {
 	 */
 	public void createTable() {
 		/*
-		 * For simplicity, just create all the tables here. 
+		 * For simplicity, just create all the tables here.
 		 */
 		dbM.queryQuiet(
 				"CREATE TABLE IF NOT EXISTS Customer(customerID INTEGER PRIMARY KEY, address STRING NOT NULL, city STRING NOT NULL,"
-				+ "zip INTEGER NOT NULL, state STRING NOT NULL, country STRING NOT NULL, phone STRING NOT NULL, email STRING NOT NULL);"
-		);
+						+ "zip INTEGER NOT NULL, state STRING NOT NULL, country STRING NOT NULL, phone STRING NOT NULL, email STRING NOT NULL);");
 		dbM.queryQuiet(
 				"CREATE TABLE IF NOT EXISTS Invoice(invoiceID INTEGER NOT NULL, customerID_FK INTEGER references Customer(customerID),"
-				+ "purchase_type STRING NOT NULL, quantity INTEGER NOT NULL, unit_price INTEGER NOT NULL, data STRING NOT NULL);"
-		);
+						+ "purchase_type STRING NOT NULL, quantity INTEGER NOT NULL, unit_price INTEGER NOT NULL, data STRING NOT NULL);");
 		dbM.queryQuiet(
 				"CREATE TABLE IF NOT EXISTS Dealership(dealershipID INTEGER PRIMARY KEY, location STRING NOT NULL, address STRING NOT NULL,"
-				+ "zip INTEGER NOT NULL, city STRING NOT NULL, state STRING NOT NULL, country STRING NOT NULL);"
-		);	
+						+ "zip INTEGER NOT NULL, city STRING NOT NULL, state STRING NOT NULL, country STRING NOT NULL);");
 		dbM.queryQuiet(
 				"CREATE TABLE IF NOT EXISTS Car(VIN STRING PRIMARY KEY, color STRING NOT NULL, buy_price INTEGER NOT NULL, lease_price INTEGER NOT NULL,"
-				+ "producer STRING NOT NULL, dealershipID_FK INTEGER references Dealership(dealershipID), customerID_FK INTEGER references Customer(customerID),"
-				+ "invoiceID_FK INTEGER references Invoice(invoiceID));"	
-		);
+						+ "producer STRING NOT NULL, dealershipID_FK INTEGER references Dealership(dealershipID), customerID_FK INTEGER references Customer(customerID),"
+						+ "invoiceID_FK INTEGER references Invoice(invoiceID));");
 		dbM.queryQuiet(
-				"CREATE TABLE IF NOT EXISTS Car_Invoice(invoiceID_PK INTEGER, VIN_PK INTEGER, PRIMARY KEY(invoiceID_PK, VIN_PK));"
-		);	
+				"CREATE TABLE IF NOT EXISTS Car_Invoice(invoiceID_PK INTEGER, VIN_PK INTEGER, PRIMARY KEY(invoiceID_PK, VIN_PK));");
 	}
 
 	/**
 	 * For testing purposes
 	 */
 	public void insertTable() {
-		dbM.queryQuiet(String.format("INSERT INTO Customer values(%d, '%s', '%s', %d, '%s', '%s', '%s', '%s');", 1,"addr","city", 11111, "ca", "US", "0161302013", "email"));
+		dbM.queryQuiet(String.format("INSERT INTO Customer values(%d, '%s', '%s', %d, '%s', '%s', '%s', '%s');", 2,
+				"addr", "city", 11111, "ca", "US", "0161302013", "email"));
 	}
 
 	/**
@@ -59,8 +54,8 @@ public class Main {
 	 */
 	public void queryTable() {
 		ResultSet rs = dbM.query("SELECT * FROM Customer;");
-		try{	
-			while(rs.next()) {
+		try {
+			while (rs.next()) {
 				System.out.println(rs.getInt("customerID"));
 				System.out.println(rs.getString("address"));
 				System.out.println(rs.getString("city"));
@@ -70,17 +65,17 @@ public class Main {
 				System.out.println(rs.getString("phone"));
 				System.out.println(rs.getString("email"));
 			}
-		}
-		catch(SQLException e) {
+		} catch (SQLException e) {
 			throw new RuntimeException(e.getMessage());
 		}
 	}
+
 	public static void main(String[] args) {
 		Main mainObj = new Main();
 		mainObj.createTable();
 		mainObj.insertTable();
 		mainObj.queryTable();
-		
+
 	}
 
 }
