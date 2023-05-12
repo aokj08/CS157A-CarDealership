8462343805782a6a17d61f4abc1cb3fe989eff6b
aokj08 <104790843+aokj08@users.noreply.github.com>
2023-05-12 15:57:02 -0700

    cleaned up Employeepage and updated todo

--- src/application/Main.java
+++ src/application/Main.java
@@ -124,8 +124,8 @@ public class Main {
 	public static void main(String[] args) {
 		Main obj = new Main();
 		// obj.insertTable();
-		obj.queryTable();
-		// new Homepage();
+		// obj.queryTable();
+		new Homepage();
 		// obj.dropAll();
 	}
 
